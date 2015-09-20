9fbbdfed3f36a3eaa2f84ab577f69ba17100a698
Xtricman <xtricman@gmail.com>
2015-09-20 21:46:57 +0800

    增加场景，人物，物品栏功能，保存时保存整个地图(以ID保存)

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-import ttyavg
+import ttyavg import *
 
-def render_and_get_event(uuid):
+def render_and_get_event(state):
     node = uuid_node_map[uuid]
     print('=====')
     print(node['description_text'])
     print()
     if not node['options']:
-        return ('QUIT',)
+        return Event('QUIT', None)
     for k in node['options']:
         print(k)
     print('=====')
     print('>>', end='')
     user_input = input()
     if user_input == 'quit':
-        return ('QUIT',)
+        return Event('QUIT', None)
     if user_input[0:4] == 'save':
-        return ('SAVE', user_input[5:])
-    if user_input == 'back':
-        return ('BACK',)
-    if int(user_input) in range(0, len(game_map[uuid])):
-        return ('CONTINUE', game_map[uuid][int(user_input)])
+        return Event('SAVE', user_input[5:])
 
 uuid_node_map = {
                  0:{'description_text':'游戏开始了', 'options':['路线1', '路线2']},
@@ -30,7 +26,7 @@ uuid_node_map = {
                  4:{'description_text':'路线2发展', 'options':['去结局']},
                  6:{'description_text':'路线2结局', 'options':None}
                 }
-game_map = {0:[1,2], 1:[3], 3:[5], 2:[4], 4:[6]}
+init_state = {0:[1,2], 1:[3], 3:[5], 2:[4], 4:[6]}
 
 ttyavg.render_and_get_event = render_and_get_event
-ttyavg.game_start()
+ttyavg.game_start(init_state)
--- ttyavg.py
+++ ttyavg.py
@@ -1,34 +1,44 @@
 import pickle
-import sys
 
+class State(dict):
+    """self["player_id"] = player_id
+self["player_inventory"] = {each player_id:{item_id}}
+self["player_positions"] = {each player_id:scene_id}
+self["scene_items"] = {each scene_id:{item_id}}"""
 
-def game_start():
-    if len(sys.argv)==1:
-        start_uuid = 0
-        runtime_history = []
-    else:
-        with open(sys.argv[-1], "rb") as savedgame:
-            start_uuid, runtime_history = pickle.load(savedgame)
+class Event(dict):
+    def __init__(self, type, arg):
+        self["type"] = type
+        self["arg"] = arg
 
-    def run(uuid):
-        runtime_history.append(uuid)
-        event = render_and_get_event(uuid)
+def game_start(init_state):
+
+    state = init_state
+
+    while(True):
+        event = render_and_get_event(state)
         
-        supported_types = ('SAVE', 'QUIT', 'CONTINUE', 'BACK')
-        if event[0] not in supported_types:
-            raise ValueError("Event type {} not supported.".format(event[0]))
+        supported_types = ('SAVE', 'QUIT', 'DELET', 'ADDINVENTORY')
+        if event.type not in supported_types:
+            raise ValueError("Event type {} not supported.".format(event.type))
 
-        while event[0] == 'SAVE':
-            with open(event[1], "wb") as savefile:
-                pickle.dump((uuid, runtime_history[:-1]), savefile)
-            event = render_and_get_event(uuid)
-        if event[0] == 'QUIT':
-            return event[1]
-        if event[0] == 'CONTINUE':
-            return run(event[1])
-        if event[0] == 'BACK':
-            if len(runtime_history) > 1:
-                runtime_history.pop()
-                return run(runtime_history.pop())
-
-    run(start_uuid)
+        if event.type == 'SAVE':
+            with open(event.arg, "wb") as save_file:
+                pickle.dump(state, save_file)
+        elif event.type == 'QUIT':
+            return event.arg
+        elif event.type == 'DELET':
+            if state.player_id == event.arg:
+                state.player_id = None
+            state.player_inventory.pop(event.arg, None)
+            state.player_positions.pop(event.arg, None)
+            state.scene_items.pop(event.arg, None)
+            for i in state.player_inventory:
+                state.player_inventory[i].discard(event.arg)
+            for i in state.scene_items:
+                state.scene_items[i].discard(event.arg)
+            for k,v in state.player_positions:
+                if v == event.arg:
+                state.player_positions[k] = None
+        elif event.type == 'ADDINVENTORY':
+            state.player_inventory[state.player_id].add(event.arg)
